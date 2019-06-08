f9c0249a6968617e696cf30dca8f2a85f1b79b03
s206e11 <s206e11@b14_206_11>
2019-06-08 10:51:44 -0500

    Commit inicial

---
+++ src/ejercicio1/CountOccurrencesInt.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+
+import java.util.List;
+
+/**
+ *
+ * @author jpramirez
+ */
+public interface CountOccurrencesInt {
+    
+    List<String> countOccurrences(List<Integer> list);
+
+}
